1df2196334e37a0c8747d12c8c0e03889288e9b6
kadenhargrove <kadenhargrove@gmail.com>
2023-02-28 17:50:06 -0600

    graph file stored in matrix

--- findClique.java
+++ findClique.java
@@ -4,11 +4,26 @@ import java.util.*;
 class findClique
 {
 
-    int[][] matrix;
+    private static int[][] matrix;
 
     public void findMaxClique(int m[][])
     {
 
+    }
+
+    //prints 2D array
+    public static void printMatrix(int mat[][])
+    {
+        //loop through rows
+        for (int i = 0; i < mat.length; i++)
+        {
+            //loop through all elements of current row
+            for (int j = 0; j < mat[i].length; j++)
+            {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args)
@@ -17,18 +32,29 @@ class findClique
             File myObj = new File("file.txt");
             Scanner scan = new Scanner(myObj);
 
+            //size read from graph file
             int size = scan.nextInt();
             String[] data2 = new String[size];
+            scan.nextLine();
 
+            //set matrix size
+            matrix = new int[size][size];
+
+            //for looping through matrix
+            int row = 0;
+
+            //loop through remaining lines of file
             while(scan.hasNextLine())
             {
-                String data = scan.nextLine();
-                data2 = data.split("\\s+");
-                
-                for(int i = 0; i < data2.length; i++)
+                String data = scan.nextLine(); //reads line from graph file
+                data2 = data.split("\\s+"); //splits white space, stores characters in array
+
+                //store line of numbers to corresponding row in matrix
+                for (int i = 0; i < size; i++)
                 {
-                    
+                    matrix[row][i] = Integer.parseInt(data2[i]); //cast string to int, store in matrix
                 }
+                row++;
             }
             scan.close();
         }
@@ -37,5 +63,7 @@ class findClique
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+
+        printMatrix(matrix); //print the stored matrix!
     }
 }
