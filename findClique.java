d68e751054c6d0d06a7161cfcb52d4bc9c98f4ab
GYonan <g2yonan@gmail.com>
2023-03-07 12:08:50 -0600

    Fixed file reading

--- findClique.java
+++ findClique.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 class findClique {
 
-    private static int[][] matrix;
+    private static ArrayList<int[][]> matrixes = new ArrayList<int[][]>(50);
     private static int[][] clique;
     private static int[] edges;
 
@@ -14,7 +14,7 @@ class findClique {
     }
 
     // find number of nodes in graph based on length of matrix
-    public static int findNodes(int m[][]) {
+    public static int findVerticies(int m[][]) {
         int nodes = m.length;
         System.out.println(nodes);
         return nodes;
@@ -37,55 +37,49 @@ class findClique {
     }
 
     // prints 2D array
-    public static void printMatrix(int mat[][]) {
+    public static void printMatrix(int[][] mat) {
         // loop through rows
         for (int i = 0; i < mat.length; i++) {
             // loop through all elements of current row
             for (int j = 0; j < mat[i].length; j++) {
-                System.out.print(mat[i][j] + " ");
+                System.out.print(mat[i][j]);
             }
             System.out.println();
         }
-    }
-
-    // function that checks if the given set of verticies in our array are a clique
-    public static boolean is_clique(int num) {
-        for (int i = 1; i < num; i++) {
-            for (int j = i + 1; j < num; j++)
-                // If any edge is missing
-                if (clique[edges[i]][edges[j]] == 0)
-                    return false;
-        }
-        return true;
+        System.out.println();
     }
 
     // input file path with undirected graphs
-    public static void saveMatrix(String pathname) {
+    public static void saveMatrixes(String pathname) {
         try {
             File myObj = new File(pathname);
             Scanner scan = new Scanner(myObj);
 
-            // size read from graph file
-            int size = scan.nextInt();
-            String[] data2 = new String[size];
-            scan.nextLine();
-
-            // set matrix size
-            matrix = new int[size][size];
-
-            // for looping through matrix
-            int row = 0;
-
-            // loop through remaining lines of file
             while (scan.hasNextLine()) {
-                String data = scan.nextLine(); // reads line from graph file
-                data2 = data.split("\\s+"); // splits white space, stores characters in array
+                // size read from graph file
+                int size = scan.nextInt();
+                String[] data2 = new String[size];
+                scan.nextLine();
 
-                // store line of numbers to corresponding row in matrix
-                for (int i = 0; i < size; i++) {
-                    matrix[row][i] = Integer.parseInt(data2[i]); // cast string to int, store in matrix
+                // set matrix size
+                int matrix[][] = new int[size][size];
+
+                // for looping through matrix
+                int row = 0;
+
+                // loop through remaining lines of file
+                while (scan.hasNextLine()) {
+                    String data = scan.nextLine(); // reads line from graph file
+                    data2 = data.split("\\s+"); // splits white space, stores characters in array
+
+                    // store line of numbers to corresponding row in matrix
+                    for (int i = 0; i < size; i++) {
+                        matrix[row][i] = Integer.parseInt(data2[i]); // cast string to int, store in matrix
+                    }
+                    row++;
+                    if(row == size) break; // break out of loop if all rows are filled
                 }
-                row++;
+                matrixes.add(matrix);
             }
             scan.close();
         } catch (FileNotFoundException e) {
@@ -95,9 +89,10 @@ class findClique {
     }
 
     public static void main(String[] args) {
-        saveMatrix("file.txt");
-        findEdges(matrix);
-        findMaxClique(findEdges(matrix), findNodes(matrix));
+        saveMatrixes("graphs.txt");
+        for(int i=0; i<matrixes.size();i++){
+            printMatrix(matrixes.get(i));
+        }
 
     }
 }
